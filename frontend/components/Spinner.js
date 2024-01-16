f68c76f59783efb957cfc5ae88ea88cbb35f6e3d
Percouple <calebsevan@gmail.com>
2024-01-16 15:51:41 -0600

    Finishing-Challenges

--- frontend/components/Spinner.js
+++ frontend/components/Spinner.js
@@ -37,22 +37,26 @@ STEP 4:
 */
 
 import React from 'react'; /* STEP 0 */
+import { useState } from 'react';
 
 export default function Spinner() {
 /* STEP 1 */
 
+let [spinnerOn, setSpinnerOn] = useState(true);
+
   const toggleSpinner = () => {
   /* STEP 4 */
+    setSpinnerOn((spinnerToggle) => !spinnerToggle)
   };
 
   return (
     <div className='widget-spinner container'>
       <h2>Spinner</h2>
       {
-        true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        spinnerOn && <div id='spinner' className='spinner'>---+---</div> /* STEP 2 */
       }
       <button id='toggleSpinner' onClick={toggleSpinner}>
-        Hide Spinner {/* STEP 3 */}
+        {spinnerOn ? "Hide" : "Show"} Spinner {/* STEP 3 */}
       </button>
     </div>
   );
